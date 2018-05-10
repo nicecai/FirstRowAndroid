d4d2dcfd00c529f130acbff5f69b823bb8e658fc
nicecai <aaron.ckj@gmail.com>
2018-05-10 21:39:16 +0800

    Init...

---
+++ 1/ActivityTest/app/src/main/java/activitytest/example/com/activitytest/FirstActivity.java
@@ -0,0 +1,91 @@
+package activitytest.example.com.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+    private static final String TAG = "FirstActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.first_layout);
+        Button button1 = (Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"You Clicked Button 1 to destory this activity.",
+                        Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: button1 clicked");
+//                finish();
+
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                startActivity(intent);
+//                startActivity(intent);
+
+
+//                Intent intent = new Intent("activitytest.example.com.activitytest.ACTION_START");
+//                intent.addCategory("activitytest.exampletytest.MY_CATEGORY");
+
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("https://www.google.com"));
+//                startActivity(intent);
+
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+//
+//                String data = "Hello SecondActivity";
+//                Integer data2 = 1314;
+//                Boolean data3 = false;
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                intent.putExtra("extra_data2",data2);
+//                intent.putExtra("extra_data3",data3);
+//                startActivity(intent);
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d(TAG, returnedData);
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You click Add_Item!",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.remove_item:
+                Toast.makeText(this,"You click Remove_Item!",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
---
+++ 1/HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    private static final String TAG = "HelloWorldActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.v("HelloWorldActivity","on Create Log");
+        Log.d("dddd", "onCreate: dddddddd");
+        Log.i("iiii", "onCreate: iiiiiiii");
+        Log.d(TAG, "onCreate: ddddddddddDDDDDDDD");
+
+    }
+}
