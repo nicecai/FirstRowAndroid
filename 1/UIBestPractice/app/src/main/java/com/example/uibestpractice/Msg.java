17f3fb4de68c6a8f63d936ea0ac25219643706d2
nicecai <aaron.ckj@gmail.com>
2018-05-20 23:12:20 +0800

    add IM Layout e.g

---
+++ 1/UIBestPractice/app/src/main/java/com/example/uibestpractice/Msg.java
@@ -0,0 +1,22 @@
+package com.example.uibestpractice;
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SEND = 1;
+    private String content;
+    private int type;
+
+    public Msg(String content, int type)
+    {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+}
